9164f9420321667c32736584fa12989e62a0ae0f
RinnAnd <andre.ksas@gmail.com>
2026-03-04 19:08:08 -0500

    feat: adding connection package

--- internal/infra/database/connection/connection.go
+++ internal/infra/database/connection/connection.go
@@ -1,1 +1,26 @@
 package connection
+
+import (
+	"database/sql"
+
+	_ "github.com/lib/pq"
+)
+
+type Config struct {
+	dsn string
+}
+
+func New(dsn string) *Config {
+	return &Config{
+		dsn: dsn,
+	}
+}
+
+func (c *Config) Open() *sql.DB {
+	conn, err := sql.Open("postgres", c.dsn)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	return conn
+}
