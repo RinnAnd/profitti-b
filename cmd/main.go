2d72f3191379ab827f082aab622893553ebe003b
Loloming <damianbruzzoni@gmail.com>
2026-03-05 23:09:34 -0300

    wawaw

--- cmd/main.go
+++ cmd/main.go
@@ -6,9 +6,16 @@ import (
 	"profitti/internal/app/transport/http/routes"
 	"profitti/internal/app/transport/server"
 	"profitti/internal/infra/database/connection"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load("../.env")
+	if err != nil {
+		print(err.Error())
+		return
+	}
 	cfg := connection.New(os.Getenv("GOOSE_DBSTRING"))
 	db := cfg.Open()
 	setup := setup.Build(db)
@@ -18,5 +25,6 @@ func main() {
 	}
 
 	server := server.StartServer(os.Getenv("PORT"))
+	rtr.Init(server.G)
 	server.Run()
 }
--- cmd/setup/users.go
+++ cmd/setup/users.go
@@ -7,7 +7,7 @@ import (
 	service "profitti/internal/infra/service/users"
 )
 
-func User(db *sql.DB, stp *Setup) *Setup {
+func User(db *sql.DB, stp *Setup) {
 	userRepository := user.New(db)
 	userService := service.New(userRepository)
 	userHandler := users.NewUserHandler(userService)
